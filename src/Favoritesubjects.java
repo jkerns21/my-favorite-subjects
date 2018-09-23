92e92c07e45370748b7169bcf10ae01c8b3a023d
jkerns21 <43002620+jkerns21@users.noreply.github.com>
2018-09-23 11:47:07 -0400

    Add files via upload

---
+++ src/Favoritesubjects.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Jonathan Kerns
+ * period 1
+ *
+ */
+public class Favoritesubjects 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("My favorite subjects:");
+		System.out.println("\tComputer Science");
+		System.out.println("\tMath");
+		System.out.println("\tScience");
+	}
+}
